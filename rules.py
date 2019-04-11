3837d53a62b0c2488b33500464a372e091336bf3
Mohamad Firdaus <36685006+Firdausjusoh27@users.noreply.github.com>
2019-04-11 13:30:18 +0800

    New Rules

--- rules.py
+++ rules.py
@@ -1,38 +1,35 @@
-def LHR(arr, ckp):
-	str1 = ''.join(arr[-3:])
-	if str1 == "LBL":
-		del arr[-3:]
-		arr.append("S")
-		ckp.append("S")
-	elif str1 == "LBR" or str1 == "RBL" or str1 == "SBS":
-		del arr[-3:]
-		arr.append("B")
-		ckp.append("B")
-	elif str1 == "LBS" or str1 == "SBL":
-		del arr[-3:]
-		arr.append("R")
-		ckp.append("R")
+def check(act):
+    str2 = ''.join(act[-3:])
+    if str2 == "LBR" or str2 == "LBS" or str2 == "LBL" or str2 == "SBL" or str2 == "SBS" or str2 == "RBL":
+        LHR(act)
+
+    else:
+        return act
+
+
+def LHR(act):
+    str1 = ''.join(act[-3:])
+    if str1 == "LBL":
+        del act[-3:]
+        act.append("S")
+        check(act)
+    if str1 == "LBR" or str1 == "RBL" or str1 == "SBS":
+        del act[-3:]
+        act.append("B")
+        check(act)
+    elif str1 == "LBS" or str1 == "SBL":
+        del act[-3:]
+        act.append("R")
+        check(act)
+    else:
+        return act
+
 
 def back_travel(arr):
-	for i in range(len(arr)):
-		if arr[i] == "L":
-			arr[i] = "R"
-		elif arr[i] == "R":
-			arr[i] = "L"	
-	arr.reverse()
-	return arr
-
-
-# def RHR(arr, ckp):
-# 	str1 = ''.join(arr[-3:])
-# 	if str1 == "RBR":
-# 		del arr[-3:]
-# 		arr.append("S")
-# 		ckp.append("S")
-# 	elif str1 == "RBL" or str1 == "LBR" or str1 == "SBS":
-# 		del arr[-3:]
-# 		arr.append("B")
-# 	elif str1 == "RBS" or str1 == "SBR":
-# 		del arr[-3:]
-# 		arr.append("L")
-# 		ckp.append("L")
+    for i in range(len(arr)):
+        if arr[i] == "L":
+            arr[i] = "R"
+        elif arr[i] == "R":
+            arr[i] = "L"
+    arr.reverse()
+    return arr
